30ac5b94d1fd09fe32c0e7dc14b9a6016ef3c8be
Daniil Kachur <daniilkachur1704@gmail.com>
2025-08-29 14:21:27 +0300

    feat: registered product services and repository to DI container

--- MinimalAPI.WebAPI/Extensions/StartupExtensions.cs
+++ MinimalAPI.WebAPI/Extensions/StartupExtensions.cs
@@ -3,6 +3,10 @@ using MinimalAPI.Infrastructure.DatabaseContext;
 using FluentValidation;
 using MinimalAPI.Application.DTOs;
 using MinimalAPI.Application.Validatiors;
+using MinimalAPI.Application.ServiceContracts;
+using MinimalAPI.Application.Services;
+using MinimalAPI.Domain.RepositoryContracts;
+using MinimalAPI.Infrastructure.Repositories;
 
 namespace MinimalAPI.WebAPI.Extensions
 {
@@ -10,6 +14,12 @@ namespace MinimalAPI.WebAPI.Extensions
     {
         public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddScoped<IProductAdderService, ProductAdderService>();
+            services.AddScoped<IProductGetterService, ProductGetterService>();
+            services.AddScoped<IProductUpdaterService, ProductUpdaterService>();
+            services.AddScoped<IProductDeleterService, ProductDeleterService>();
+            services.AddScoped<IProductsRepository, ProductsRepository>();
+
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("Default"));
