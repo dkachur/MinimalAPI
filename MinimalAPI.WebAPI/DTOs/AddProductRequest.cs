bdbf8ae7aabeada17117ae900e84fab2632af489
Daniil Kachur <daniilkachur1704@gmail.com>
2025-08-29 14:22:28 +0300

    feat: add DTOs for add product request and response

---
+++ MinimalAPI.WebAPI/DTOs/AddProductRequest.cs
@@ -0,0 +1,8 @@
+﻿using Mapster;
+using MinimalAPI.Application.DTOs;
+
+namespace MinimalAPI.WebAPI.DTOs
+{
+    [AdaptTo(typeof(AddProductDto)), GenerateMapper]
+    public record AddProductRequest(string Name, string Description, decimal Price);
+}
---
+++ MinimalAPI.WebAPI/DTOs/ProductResponse.cs
@@ -0,0 +1,8 @@
+﻿using Mapster;
+using MinimalAPI.Application.DTOs;
+
+namespace MinimalAPI.WebAPI.DTOs
+{
+    [AdaptFrom(typeof(ProductDto)), GenerateMapper]
+    public record ProductResponse(Guid Id, string Name, string Description, decimal Price);
+}
