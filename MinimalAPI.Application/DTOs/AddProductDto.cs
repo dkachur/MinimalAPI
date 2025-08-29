5bc012ee58dbcdf50f07d15b35d9d5cec84f8d62
Daniil Kachur <daniilkachur1704@gmail.com>
2025-08-29 14:08:57 +0300

    refactor: convert AddProductDto from class to record

--- MinimalAPI.Application/DTOs/AddProductDto.cs
+++ MinimalAPI.Application/DTOs/AddProductDto.cs
@@ -4,10 +4,5 @@ using MinimalAPI.Domain.Entities;
 namespace MinimalAPI.Application.DTOs
 {
     [AdaptTo(typeof(Product)), GenerateMapper]
-    public class AddProductDto
-    {
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-        public required decimal Price { get; set; }
-    }
+    public record AddProductDto(string Name, string Description, decimal Price);
 }
